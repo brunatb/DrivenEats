43e734648af9345b46341d31ea7c59f19585a20c
Bruna Topan <brutopanb@gmail.com>
2021-08-25 15:03:24 -0300

    Fixed bottom

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <button>Selecione os 3 itens<br />para fechar o pedido</button>
+    </footer>
+  );
+}
+
+export default Footer;
